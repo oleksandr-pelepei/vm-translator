4e2cfb4264c539c8cdf934fd0369ceb3aa501117
Oleksandr <sasha.pelepey@gmail.com>
2020-06-08 14:48:05 +0300

    feat(commands) - implement LtCommand

---
+++ src/commands/lt-command.js
@@ -0,0 +1,42 @@
+const {ArithmeticCommand} = require('./arithmetic-command');
+
+class LtCommand extends ArithmeticCommand {
+  static counter = 0;
+
+  constructor() {
+    super();
+
+    LtCommand.counter++;
+  }
+
+  getOperationCode() {
+    const pointerXCode = `
+      @SP
+      A=M 
+      A=A-1 // x
+    `;
+
+    return `
+      D=M-D // D = *x - *y
+        
+      @LT_ELSE_${LtCommand.counter}
+      D=D;JGT // *x < *y
+      
+      ${pointerXCode}
+      M=1 // *x = *x < *y = true
+      
+      @LT_END_${LtCommand.counter}
+      0;JMP
+      
+      (LT_ELSE_${LtCommand.counter})
+      ${pointerXCode}
+      M=0 // *x = *x < *y = false
+      
+      (LT_END_${LtCommand.counter})
+    `;
+  }
+}
+
+module.exports = {
+  LtCommand
+}
---
+++ src/commands/lt-command.test.js
@@ -0,0 +1,41 @@
+const {LtCommand} = require('./lt-command');
+const {prettifyAssemblyCode} = require('../../tests/utils');
+
+describe('LtCommand', () => {
+  it('should be defined', () => {
+    expect(new LtCommand()).toBeDefined();
+  });
+
+  describe('Method getOperationCode()', () => {
+    it('should return assembly code which compares x < y', () => {
+      const command = new LtCommand();
+      const counter = LtCommand.counter;
+      const exactedCode = prettifyAssemblyCode(`
+        D=M-D // D = *x - *y
+        
+        @LT_ELSE_${counter}
+        D=D;JGT // *x < *y
+        
+        @SP
+        A=M 
+        A=A-1 // x
+        M=1 // *x = *x < *y = true
+        
+        @LT_END_${counter}
+        0;JMP
+        
+        (LT_ELSE_${counter})
+        @SP
+        A=M 
+        A=A-1 // x
+        M=0 // *x = *x < *y = false
+        
+        (LT_END_${counter})
+      `);
+
+      const resultCode = prettifyAssemblyCode(command.getOperationCode());
+
+      expect(resultCode).toBe(exactedCode);
+    });
+  });
+});
