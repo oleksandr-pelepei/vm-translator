c71e5aaa75cae989bd16ae480dbac04761313d83
Oleksandr <sasha.pelepey@gmail.com>
2020-06-08 14:42:37 +0300

    feat(commands) - implement GtCommand

---
+++ src/commands/gt-command.js
@@ -0,0 +1,42 @@
+const {ArithmeticCommand} = require('./arithmetic-command');
+
+class GtCommand extends ArithmeticCommand {
+  static counter = 0;
+
+  constructor() {
+    super();
+
+    GtCommand.counter++;
+  }
+
+  getOperationCode() {
+    const pointerXCode = `
+      @SP
+      A=M 
+      A=A-1 // x
+    `;
+
+    return `
+      D=M-D // D = *x - *y
+        
+      @GT_ELSE_${GtCommand.counter}
+      D=D;JLT // *x > *y
+      
+      ${pointerXCode}
+      M=1 // *x = *x > *y = true
+      
+      @GT_END_${GtCommand.counter}
+      0;JMP
+      
+      (GT_ELSE_${GtCommand.counter})
+      ${pointerXCode}
+      M=0 // *x = *x > *y = false
+      
+      (GT_END_${GtCommand.counter})
+    `;
+  }
+}
+
+module.exports = {
+  GtCommand
+}
---
+++ src/commands/gt-command.test.js
@@ -0,0 +1,41 @@
+const {GtCommand} = require('./gt-command');
+const {prettifyAssemblyCode} = require('../../tests/utils');
+
+describe('GtCommand', () => {
+  it('should be defined', () => {
+    expect(new GtCommand()).toBeDefined();
+  });
+
+  describe('Method getOperationCode()', () => {
+    it('should return assembly code which compares x > y', () => {
+      const command = new GtCommand();
+      const counter = GtCommand.counter;
+      const exactedCode = prettifyAssemblyCode(`
+        D=M-D // D = *x - *y
+        
+        @GT_ELSE_${counter}
+        D=D;JLT // *x > *y
+        
+        @SP
+        A=M 
+        A=A-1 // x
+        M=1 // *x = *x > *y = true
+        
+        @GT_END_${counter}
+        0;JMP
+        
+        (GT_ELSE_${counter})
+        @SP
+        A=M 
+        A=A-1 // x
+        M=0 // *x = *x > *y = false
+        
+        (GT_END_${counter})
+      `);
+
+      const resultCode = prettifyAssemblyCode(command.getOperationCode());
+
+      expect(resultCode).toBe(exactedCode);
+    });
+  });
+});
